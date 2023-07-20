4e32ed6af5e2108906ef99717d18ab2dd902ecfe
Miku_Snow <gal499605076@gmail.com>
2023-07-20 23:26:35 +0800

    新增重载资源包、材质包指令/geyser loadpacks

---
+++ core/src/main/java/org/geysermc/geyser/command/defaults/LoadPacksCommand.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2019-2022 GeyserMC. http://geysermc.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author GeyserMC
+ * @link https://github.com/GeyserMC/Geyser
+ */
+
+package org.geysermc.geyser.command.defaults;
+
+import org.geysermc.geyser.GeyserImpl;
+import org.geysermc.geyser.command.GeyserCommand;
+import org.geysermc.geyser.command.GeyserCommandSource;
+import org.geysermc.geyser.pack.BehaviorPack;
+import org.geysermc.geyser.pack.ResourcePack;
+import org.geysermc.geyser.session.GeyserSession;
+
+public class LoadPacksCommand extends GeyserCommand {
+
+    private final GeyserImpl geyser;
+
+    public LoadPacksCommand(GeyserImpl geyser, String name, String description, String permission) {
+        super(name, description, permission);
+        this.geyser = geyser;
+    }
+
+    @Override
+    public void execute(GeyserSession session, GeyserCommandSource sender, String[] args) {
+        if (!sender.isConsole()) {
+            return;
+        }
+
+        String message = "正在重载材质包！";
+
+
+        ResourcePack.loadPacks();
+        BehaviorPack.loadPacks();
+
+        sender.sendMessage(message);
+    }
+
+    @Override
+    public boolean isSuggestedOpOnly() {
+        return true;
+    }
+}
