7edcd3b8b56663b62ab397f8e95d582263ca41fc
SamB440 <sam@islandearth.net>
2026-01-09 17:05:48 +0000

    Add full support for Java ping pong packets (#5460)
    * Add full support for Java ping pong packets
    
    * Address reviews
    
    * Fixed duplicated comments.
    
    * Fixed old config code.
    
    ---------
    
    Co-authored-by: onebeastchris <github@onechris.mozmail.com>
    Co-authored-by: oryxel <gamingnoob681@gmail.com>
    (cherry picked from commit 5312213215c515dc593ffc3a872fdeb2f2bbd5a3)

--- core/src/main/java/org/geysermc/geyser/session/GeyserSession.java
+++ core/src/main/java/org/geysermc/geyser/session/GeyserSession.java
@@ -95,6 +95,7 @@ import org.cloudburstmc.protocol.bedrock.packet.GameRulesChangedPacket;
 import org.cloudburstmc.protocol.bedrock.packet.ItemComponentPacket;
 import org.cloudburstmc.protocol.bedrock.packet.LevelEventPacket;
 import org.cloudburstmc.protocol.bedrock.packet.LevelSoundEventPacket;
+import org.cloudburstmc.protocol.bedrock.packet.NetworkStackLatencyPacket;
 import org.cloudburstmc.protocol.bedrock.packet.PlayStatusPacket;
 import org.cloudburstmc.protocol.bedrock.packet.RemoveEntityPacket;
 import org.cloudburstmc.protocol.bedrock.packet.SetCommandsEnabledPacket;
@@ -769,10 +770,10 @@ public class GeyserSession implements GeyserConnection, GeyserCommandSource {
     private boolean mounting = false;
 
     /**
-     * A cache of IDs from ClientboundKeepAlivePackets that have been sent to the Bedrock client, but haven't been returned to the server.
+     * A cache of IDs from ClientboundKeepAlivePackets or ClientboundPingPacket that have been sent to the Bedrock client, but haven't been returned to the server.
      * Only used if {@link GeyserConfig.GameplayConfig#forwardPlayerPing()} is enabled.
      */
-    private final Queue<Long> keepAliveCache = new ConcurrentLinkedQueue<>();
+    private final Queue<Runnable> latencyPingCache = new ConcurrentLinkedQueue<>();
 
     /**
      * Queued packets to be sent immediately at the end of each tick.
@@ -2623,6 +2624,18 @@ public class GeyserSession implements GeyserConnection, GeyserCommandSource {
         sendUpstreamPacket(packet);
     }
 
+    public void sendNetworkLatencyStackPacket(long timestamp, boolean ensureEventLoop, Runnable runnable) {
+        NetworkStackLatencyPacket latencyPacket = new NetworkStackLatencyPacket();
+        latencyPacket.setFromServer(true);
+        latencyPacket.setTimestamp(timestamp);
+        if (ensureEventLoop) {
+            latencyPingCache.add(() -> ensureInEventLoop(runnable));
+        } else {
+            latencyPingCache.add(runnable);
+        }
+        sendUpstreamPacket(latencyPacket);
+    }
+  
     public String getDebugInfo() {
         return "Username: %s, DeviceOs: %s, Version: %s".formatted(bedrockUsername(), platform(), version());
     }
