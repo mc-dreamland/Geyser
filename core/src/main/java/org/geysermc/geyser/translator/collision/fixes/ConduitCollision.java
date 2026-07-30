d3631dff423dfff522fa965518bf06ba8c8cf20d
bjd <dev@bjd-mc.com>
2026-07-30 13:41:18 +0800

    fix(collision): 修复玩家碰撞校正和速度状态
    重构方块碰撞推出逻辑，避免异常上抬或取消移动导致的位置不同步。
    记录客户端垂直碰撞状态，并在重生和维度切换时清空玩家速度，
    防止旧速度影响落地与碰撞判断。(#f518aa57f400)

--- core/src/main/java/org/geysermc/geyser/translator/collision/fixes/ConduitCollision.java
+++ core/src/main/java/org/geysermc/geyser/translator/collision/fixes/ConduitCollision.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2026 GeyserMC. http://geysermc.org
+ * Copyright (c) 2025 GeyserMC. http://geysermc.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,21 +23,29 @@
  * @link https://github.com/GeyserMC/Geyser
  */
 
-package org.geysermc.geyser.translator.collision;
+package org.geysermc.geyser.translator.collision.fixes;
 
 import lombok.EqualsAndHashCode;
 import org.geysermc.geyser.level.block.type.BlockState;
 import org.geysermc.geyser.level.physics.BoundingBox;
+import org.geysermc.geyser.level.physics.CollisionManager;
+import org.geysermc.geyser.level.physics.Direction;
+import org.geysermc.geyser.session.GeyserSession;
+import org.geysermc.geyser.translator.collision.BlockCollision;
+import org.geysermc.geyser.translator.collision.CollisionRemapper;
 
 @EqualsAndHashCode(callSuper = true)
-@CollisionRemapper(regex = "_fence$|_wall$", passDefaultBoxes = true)
-public final class FenceAndWallCollision extends BlockCollision {
-    public FenceAndWallCollision(BlockState state, BoundingBox[] defaultBoxes) {
-        super(defaultBoxes);
+@CollisionRemapper(regex = "^conduit$", passDefaultBoxes = true)
+public class ConduitCollision extends BlockCollision {
+    private final static double MAX_PUSH_DISTANCE = 0.1875 + CollisionManager.COLLISION_TOLERANCE * 1.01;
+
+    public ConduitCollision(BlockState state, BoundingBox[] boxes) {
+        super(boxes);
     }
 
     @Override
-    protected boolean canCorrectPositionUp(int x, int y, int z, BoundingBox playerCollision) {
-        return isPlayerCenterInsideHorizontalBounds(x, z, playerCollision);
+    protected void correctPosition(GeyserSession session, int x, int y, int z, BoundingBox blockCollision, BoundingBox playerCollision) {
+        // Check for conduit bug (conduit is lifted from the ground on Java unlike Bedrock where conduit is placed on the ground)
+        blockCollision.pushOutOfBoundingBox(playerCollision, Direction.UP, MAX_PUSH_DISTANCE);
     }
 }
